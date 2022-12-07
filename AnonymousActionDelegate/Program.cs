b88e37feaf100c107c76f1d32f3f6aa9b151f5da
nandagopal.rn <nandagopal.rn@PIA3278>
2022-12-07 16:49:32 +0530

    Add project files.

---
+++ AnonymousActionDelegate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AnonymousActionDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int, int> SumAction = delegate (int firstNumber, int secondNumber)
+            {
+                Console.WriteLine($"Sum is {firstNumber + secondNumber}");
+            };
+            Action<int, int> DifferenceAction = delegate (int firstNumber, int secondNumber)
+            {
+                Console.WriteLine($"Difference is {firstNumber - secondNumber}");
+            };
+            Action<int, int> MultiplyAction = delegate (int firstNumber, int secondNumber)
+            {
+                Console.WriteLine($"Product is {firstNumber * secondNumber}");
+            };
+            Action<int, int> DivisionAction = delegate (int firstNumber, int secondNumber)
+            {
+                Console.WriteLine($"Quotient is {firstNumber / secondNumber}");
+            };
+
+            SumAction(20, 20);
+            DifferenceAction(20, 20);
+            MultiplyAction(20, 20);
+            DivisionAction(20, 20);
+        }
+    }
+}
