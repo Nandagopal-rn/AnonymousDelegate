74cd5cfb1764572ed24c296517bac2662cd3d38a
Nandagopal-rn <119663637+Nandagopal-rn@users.noreply.github.com>
2022-12-07 16:55:26 +0530

    Add files via upload

---
+++ AnonymousDelegate/AnonymousDelegate/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+namespace AnonymousDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Func<int, int, int> sumFunc = delegate (int firstNumber, int secondNumber)
+            {
+                return firstNumber + secondNumber;
+            };
+            Func<int, int, int> DifferenceFunc = delegate (int firstNumber, int secondNumber)
+            {
+                return firstNumber - secondNumber;
+            };
+            Func<int, int, int> MultiplyFunc = delegate (int firstNumber, int secondNumber)
+            {
+                return firstNumber * secondNumber;
+            };
+            Func<int, int, int> DivisionFunc = delegate (int firstNumber, int secondNumber)
+            {
+                return firstNumber / secondNumber;
+            };
+
+
+            Console.WriteLine($"Sum is {sumFunc(20, 10)}");
+            Console.WriteLine($"Difference is {DifferenceFunc(20, 10)}");
+            Console.WriteLine($"Product is {MultiplyFunc(20, 10)}");
+            Console.WriteLine($"Quotient is {DivisionFunc(20, 10)}");
+        }
+        
+
+    }
+}
