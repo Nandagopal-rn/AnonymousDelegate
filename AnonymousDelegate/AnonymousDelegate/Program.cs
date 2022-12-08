a125cf58755534aa08f6a380036ee1f742dd14ec
Nandagopal R <nandagopal.rn@pitsolutions.com>
2022-12-08 11:21:02 +0530

    name correction

--- AnonymousDelegate/AnonymousDelegate/Program.cs
+++ AnonymousDelegate/AnonymousDelegate/Program.cs
@@ -12,24 +12,24 @@ namespace AnonymousDelegate
             {
                 return firstNumber + secondNumber;
             };
-            Func<int, int, int> DifferenceFunc = delegate (int firstNumber, int secondNumber)
+            Func<int, int, int> differenceFunc = delegate (int firstNumber, int secondNumber)
             {
                 return firstNumber - secondNumber;
             };
-            Func<int, int, int> MultiplyFunc = delegate (int firstNumber, int secondNumber)
+            Func<int, int, int> multiplyFunc = delegate (int firstNumber, int secondNumber)
             {
                 return firstNumber * secondNumber;
             };
-            Func<int, int, int> DivisionFunc = delegate (int firstNumber, int secondNumber)
+            Func<int, int, int> divisionFunc = delegate (int firstNumber, int secondNumber)
             {
                 return firstNumber / secondNumber;
             };
 
 
             Console.WriteLine($"Sum is {sumFunc(20, 10)}");
-            Console.WriteLine($"Difference is {DifferenceFunc(20, 10)}");
-            Console.WriteLine($"Product is {MultiplyFunc(20, 10)}");
-            Console.WriteLine($"Quotient is {DivisionFunc(20, 10)}");
+            Console.WriteLine($"Difference is {differenceFunc(20, 10)}");
+            Console.WriteLine($"Product is {multiplyFunc(20, 10)}");
+            Console.WriteLine($"Quotient is {divisionFunc(20, 10)}");
         }
         
 
